be07e9e483c048e1d731b645f5dd0bc70c65f60d
carlafaes <carlafaes18@hotmail.com>
2022-07-25 01:40:54 -0300

    formik

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,68 +1,114 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Col from 'react-bootstrap/Col';
+import { Formik } from 'formik';
+
+// import InputGroup from 'react-bootstrap/InputGroup';
+// import Col from 'react-bootstrap/Col';
 
 
 export default function Register() {
-    const [validated, setValidated] = useState(false);
 
-    const handleSubmit = (event) => {
-      const form = event.currentTarget;
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-  
-      setValidated(true);
-    };
+    const validations = (values) => {
+        const errors = {};
+        if (!values.email) {
+            errors.email = 'Required';
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+            errors.email = 'Invalid email address';
+        }
+
+        if (!values.username) {
+            errors.username = 'Required';
+        }
+        if (!values.password) {
+            errors.password = 'Required';
+        }
+        else if (/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(values.password) === false) {
+            errors.password = 'Password must be at least 8 characters, contain at least one uppercase letter, one lowercase letter, one number and one special character';
+        }
+        if (values.password2 !== values.password) {
+            errors.password2 = 'Passwords must match';
+        }
+        return errors;
+    }
 
     return (
         <div>
             <h1>Registro</h1>
-            <Form as={Col} md="4" >
-                <Form.Group  controlId="validationCustomUsername">
-                    <Form.Label>Username</Form.Label>
-                    <InputGroup hasValidation>
-                        <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+            <Formik
+                initialValues={{
+                    username: '',
+                    email: '',
+                    password: '',
+                    password2: ''
+                }}
+                validate={validations}
+            >
+
+                {({
+                    handleSubmit,
+                    handleChange,
+                    handleBlur,
+                    values,
+                    errors,
+                    touched }) => (
+                    <Form onSubmit={handleSubmit}>
                         <Form.Control
-                            type="text"
+                            name="username"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.username}
                             placeholder="Username"
-                            aria-describedby="inputGroupPrepend"
-                            required
+                            aria-label="Username"
+                            aria-describedby="basic-addon1"
                         />
-                        <Form.Control.Feedback type="invalid">
-                            Please choose a username.
-                        </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
+                        {errors.username && touched.username && errors.username}
+                        <Form.Group
+                            className="mb-3"
+                            controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                name="email"
+                                type="email"
+                                placeholder="Enter email"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.email} />
+                        </Form.Group>
+                        {errors.email && touched.email && errors.email}
 
-                <Form.Group  className="mb-2" controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword1">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                placeholder="Password"
+                                type="password"
+                                name="password"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.password} />
+                            {errors.password && touched.password && errors.password}
+                        </Form.Group>
 
-                <Form.Group  className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword2">
+                            <Form.Label>Repeat Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                name="password2"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.password2} />
+                            {errors.password2 && touched.password2 && errors.password2}
+                        </Form.Group>
 
-                <Form.Group  className="mb-3" controlId="formBasicPassword2">
-                    <Form.Label>Repite la contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-
-                <Form.Group  className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button  variant="primary" type="submit">
-                    Registrarse
-                </Button>
-            </Form>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
