637c3c80147ceb01708f10334c57112d134f6a40
carlafaes <carlafaes18@hotmail.com>
2022-07-24 23:50:33 -0300

    ff

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {Route,Routes} from 'react-router-dom';
 //components
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Chat from './pages/Chat';
 
 function App() {
   return (
     <div className="App">
      <Routes>
+      <Route path="/" element={<Chat/>} />
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
      </Routes>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React,{useState} from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Col from 'react-bootstrap/Col';
+
 
 export default function Register() {
-    return(
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+  
+      setValidated(true);
+    };
+
+    return (
         <div>
-            <h1>Register</h1>
+            <h1>Registro</h1>
+            <Form as={Col} md="4" >
+                <Form.Group  controlId="validationCustomUsername">
+                    <Form.Label>Username</Form.Label>
+                    <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                        <Form.Control
+                            type="text"
+                            placeholder="Username"
+                            aria-describedby="inputGroupPrepend"
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please choose a username.
+                        </Form.Control.Feedback>
+                    </InputGroup>
+                </Form.Group>
+
+                <Form.Group  className="mb-2" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group  className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group  className="mb-3" controlId="formBasicPassword2">
+                    <Form.Label>Repite la contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group  className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button  variant="primary" type="submit">
+                    Registrarse
+                </Button>
+            </Form>
         </div>
     )
 }
