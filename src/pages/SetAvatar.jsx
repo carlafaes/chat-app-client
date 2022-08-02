45a232a09bfbc248f3898de61b68a8ebe6bc13d9
carlafaes <carlafaes18@hotmail.com>
2022-08-02 20:41:08 -0300

    data buffer

--- src/pages/SetAvatar.jsx
+++ src/pages/SetAvatar.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+import { Buffer } from "buffer";
+import { setAvatarRoute } from "../utils/routes/APIRoutes";
+import { api } from "../utils/avatarsAPI/avatarsAPI";
+
 
 export default function SetAvatar() {
-  return <div>SetAvatar</div>;
+    const navigate= useNavigate();
+    const [avatar, setAvatar]= useState([]);
+    const [avatarSelected, setAvatarSelected]= useState(undefined);
+    const [loading, setLoading] = useState(false);
+
+    console.log(avatar,'data avatar');
+
+    const getInfoAv=async () => {
+        const data=[];
+        console.log(data,'data');
+        let i=0;
+        do {
+            const image=  await axios.get(api);
+            const buffer= new Buffer(image.data); //convertir la imagen a buffer
+            const base64= buffer.toString('base64');//convertir el buffer a base64
+            data.push(base64);
+            setAvatar(data);
+            i++;
+        } while (i < 5); 
+    }
+    
+    useEffect(()=>{
+        getInfoAv();
+    }, []);
+
+  return(
+    <div className="container">
+        <p>Elige tu avatar</p>
+    </div>
+  );
 }
---
+++ src/utils/avatarsAPI/avatarsAPI.js
@@ -0,0 +1,4 @@
+const random=Math.floor(Math.random()*6)+1;
+const api= `https://api.multiavatar.com/${random}`;
+
+export {api};
