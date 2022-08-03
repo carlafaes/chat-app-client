5d047283c9ef81f97b6a13176d325aa76ad42611
carlafaes <carlafaes18@hotmail.com>
2022-08-03 16:29:33 -0300

    chat contacts setContacts

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function Chat() {
+const navigate=useNavigate();
+const [contacts, setContacts]= useState([]);
+const [currentUser, setCurrentUser]= useState(undefined);
+
+useEffect( async() => {
+    if(!localStorage.getItem('token-chatapp-user')){
+        navigate('/login');
+    }
+} ,[]);
+
+useEffect( async() => {
+    if(currentUser){
+        if(currentUser.isAvatarImageSet){
+            const data= await axios.get(`${allUsersRoute}/${currentUser.id}`);
+            setContacts(data.data);
+        }
+        else{
+            navigate('/setAvatar')
+        }
+    }
+},[]);
+
     return(
         <div>
             <h1>Chat</h1>
