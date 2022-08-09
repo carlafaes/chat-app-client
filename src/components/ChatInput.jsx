c6c8d16a458aca2c9848e27a838d6a70e33cd5d6
carlafaes <carlafaes18@hotmail.com>
2022-08-09 02:54:33 -0300

    emoji

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -2,11 +2,22 @@ import React,{useState} from "react";
 import Picker from 'emoji-picker-react';
 
 export default function ChatInput(){
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+    const [msg, setMsg] = useState('');
+
+    const handleEmojiPicker = () => {
+        setShowEmojiPicker(!showEmojiPicker);
+    }
 
     return(
         <div>
+            <div className="emoji-picker">
+                <button onClick={handleEmojiPicker}>
+                    Emoji{showEmojiPicker && <Picker/>}
+                </button>
+            </div>
             <form>
-        
+
             <input type="text" placeholder="Escribe un mensaje..." />
             </form>
         </div>
