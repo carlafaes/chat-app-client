c5a477698683e8cecd977c6ea319932fce4efeb2
carlafaes <carlafaes18@hotmail.com>
2022-08-09 21:24:40 -0300

    some styles

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 //route
@@ -8,41 +8,68 @@ import { getAllMessagesRoute, sendMessageRoute } from "../utils/routes/APIRoutes
 import ChatInput from "./ChatInput";
 import Logout from "./Logout";
 
-export default function ChatContainer({ currentUser,currentChat }) {
+//styles
+import s from '../styles/chatContainer.module.css';
+
+
+export default function ChatContainer({ currentUser, currentChat }) {
     const [messages, setMessages] = useState([]);
 
-const getMessages= async ()=>{
-    const response= await axios.post(getAllMessagesRoute, {
-        from:currentUser._id,
-        to:currentChat._id
-    })
-    setMessages(response.data);
-}
-    useEffect(()=>{
-        getMessages();
-    },[currentChat])
 
-    const handleSendMsg=async (msg)=>{
+    useEffect(() => {
+       
+        const getMessages = async () => {
+            const data = await JSON.parse(
+                localStorage.getItem('token-chatapp-user')
+              );
+            const response = await axios.post(getAllMessagesRoute, {
+                from: data._id,
+                to: currentChat._id
+            })
+            console.log(response, 'response')
+            setMessages(response.data);
+        }
+        getMessages();
+        if(messages.length > 0){
+        }
+        
+    }, [currentChat])
+    
+    const handleSendMsg = async (msg) => {
         await axios.post(sendMessageRoute, {
             from: currentUser._id,
             to: currentChat._id,
             message: msg
         })
+        console.log(msg, 'message')
     }
 
-  return (
-    <div>
-      <h1>Chat</h1>
-        {currentChat && (
-            <div>
-                <h1>{currentChat.username}</h1>
+    return (
+        <div>
+            <h1>Chat</h1>
+            {currentChat && (
                 <div>
-                    <img src={`data:image/svg+xml;base64,${currentChat.avatarImage}`} alt='img_perfil_cht' width='30' height='30' />
+                    <h1>{currentChat.username}</h1>
+                    <div>
+                        <img src={`data:image/svg+xml;base64,${currentChat.avatarImage}`} alt='img_perfil_cht' width='30' height='30' />
+                    </div>
+                    <div>
+                        {messages && messages.map((msg, index) => {
+                            return (
+                                <div key={index} className={`message ${msg.fromSelf ? s.sended : s.recieved}`}>
+                                    <p>
+                                        {/* {msg.from === currentUser._id ? "You" : currentChat.username} */}
+                                        {msg.message}
+                                    </p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <ChatInput handleSendMsg={handleSendMsg} />
+                    <Logout />
+
                 </div>
-                <ChatInput handleSendMsg={handleSendMsg}/>
-                <Logout/>
-            </div>
-        )}
-    </div>
-  );
+            )}
+        </div>
+    );
 }
