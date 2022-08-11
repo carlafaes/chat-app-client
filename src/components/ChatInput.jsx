0e2d2e0a1f5b63c265fd5ab938f58fd73ba71ce7
carlafaes <carlafaes18@hotmail.com>
2022-08-11 18:35:53 -0300

    tt

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -41,7 +41,7 @@ export default function ChatInput({ handleSendMsg }) {
             value={msg}
             onChange= {(e)=> setMsg(e.target.value)} 
             />
-            <button type="submit" class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            <button type="submit" className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
                 Enviar
             </button>
             </form>
